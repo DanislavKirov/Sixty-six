2c71a1065d394aefa4e06218fb091361df6e8e6f
DanislavKirov <danislav.kirov@gmail.com>
2017-01-25 23:51:20 +0200

    add more constants

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,9 +1,32 @@
 package main
 
 const (
-	player1     = 0
-	player2     = 1
-	Connect     = "connect"
-	yourTurn    = "It's your turn, pick a card index: "
-	notYourTurn = "It's your opponent's turn, please wait."
+	Player1 = 0
+	Player2 = 1
+
+	NoCard = ""
+	Card   = 0
+	Suit   = 1
+
+	// client -> server
+	Connect = "Connect"
+	Close   = "Close"
+	Quit    = "Quit"
+
+	// server -> client
+	Waiting        = "Waiting for the other player to connect.\n"
+	EnoughPlayers  = "Already enough players.\n"
+	Unknown        = "Unknown command.\n"
+	Start          = "The game starts now.\n\n"
+	YourTurn       = "It's your turn, pick a card number: "
+	NotYourTurn    = "It's your opponent's turn, please wait.\n"
+	OpponentCard   = "Opponent's card: "
+	OpponentLeft   = "Opponent left.\n"
+	OpponentClosed = "Opponent closef.\n"
+	YourHand       = "Your hand: "
+	Trump          = "Trump: "
+	TryAgain       = "Something went wrong! Please try again."
+	WrongInput     = "Wrong input, try again: "
+	Win            = "You win.\n"
+	Lose           = "You lose.\n"
 )
