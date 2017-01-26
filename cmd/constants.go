53b8dea43958bcfab786c3956a1bef8574ec58c8
DanislavKirov <danislav.kirov@gmail.com>
2017-01-26 16:18:00 +0200

    fix typo

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,3 +1,5 @@
+// This file contains the messages needed for
+// the communication between the server and the clients.
 package main
 
 const (
@@ -9,20 +11,21 @@ const (
 	Suit   = 1
 
 	// client -> server
-	Connect = "Connect"
-	Close   = "Close"
-	Quit    = "Quit"
+	Connect = "connect"
+	Close   = "close"
+	Quit    = "quit"
 
 	// server -> client
 	Waiting        = "Waiting for the other player to connect.\n"
 	EnoughPlayers  = "Already enough players.\n"
 	Unknown        = "Unknown command.\n"
 	Start          = "The game starts now.\n\n"
-	YourTurn       = "It's your turn, pick a card number: "
+	YourTurn       = "It's your turn, pick a card number or write command: "
 	NotYourTurn    = "It's your opponent's turn, please wait.\n"
 	OpponentCard   = "Opponent's card: "
 	OpponentLeft   = "Opponent left.\n"
-	OpponentClosed = "Opponent closef.\n"
+	OpponentClosed = "Opponent closed.\n"
+	AlreadyClosed  = "Already closed.\n"
 	YourHand       = "Your hand: "
 	Trump          = "Trump: "
 	TryAgain       = "Something went wrong! Please try again."
