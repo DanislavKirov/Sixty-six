2f7acb1e878976ef0c2cd6d89785746b22694036
DanislavKirov <danislav.kirov@gmail.com>
2017-02-05 00:39:14 +0200

    add a few more

--- cmd/constants.go
+++ cmd/constants.go
@@ -5,32 +5,39 @@ package main
 const (
 	Player1 = 0
 	Player2 = 1
+	Nobody  = 2
 
 	NoCard = ""
 	Rank   = 0
 	Suit   = 1
 
+	LastTrickBonus = 10
+
 	// client -> server
 	Connect  = "connect"
 	Exchange = "exchange"
 	Close    = "close"
+	Stop     = "stop"
+	Help     = "help"
 	Quit     = "quit"
 
 	// server -> client
 	Waiting           = "Waiting for the other player to connect.\n"
-	EnoughPlayers     = "Already enough players.\n"
 	Start             = "The game starts now.\n\n"
-	YourTurn          = "It's your turn, pick a card number or write command: "
-	NotYourTurn       = "It's your opponent's turn, please wait.\n"
+	YourTurn          = "It's your turn, pick a card number (1-6) or write a command: "
+	OpponentTurn      = "It's your opponent's turn, please wait.\n"
 	OpponentCard      = "Opponent's card: "
 	OpponentLeft      = "Opponent left.\n"
 	OpponentClosed    = "Opponent closed.\n"
 	OpponentExchanged = "Opponent exchanged the trump.\n"
-	YourHand          = "Your hand: "
-	Trump             = "Trump: "
 	TryAgain          = "Something went wrong! Please try again."
 	WrongInput        = "Wrong input, try again: "
-	Win               = "You win.\n"
-	Lose              = "You lose.\n"
+	WonTrick          = "You won this trick.\n"
+	WonDeal           = "You won this deal. Points: "
+	WonGame           = "YOU WON THE GAME!\n"
+	LostTrick         = "You lost this trick.\n"
+	LostDeal          = "You lost this deal. Opponents gets: "
+	LostGame          = "You lost the game.\n"
 	NotPossible       = "Operation not possible. Try something else: "
+	Commands          = "Commands:\n* exchange\n* close\n* stop\n* quit\n"
 )
