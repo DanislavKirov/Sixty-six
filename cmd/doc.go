651f2d9b51487d6c3a15611e17e7125a35ba742c
DanislavKirov <danislav.kirov@gmail.com>
2017-02-16 20:11:20 +0200

    add new files' documentation

--- cmd/doc.go
+++ cmd/doc.go
@@ -1,11 +1,14 @@
 /*
-Package main contains three files: constants, server and client.
+Package main contains five files: game.go, server.go, client.go, bot.go, constants.go.
 
-Constants provides the messages needed for the communication between
-the server and the clients.
+game.go provides api for creating and managing a game of santase.
 
-Client interacts with the player and communicates with the server.
+server.go is responsible the communication between the players and manages the game.
 
-Server is responsible for the rules of the game.
+client.go interacts with the player and communicates with the server.
+
+bot.go is responsible for the singleplayer part of the game.
+
+constants.go contains the messages used for communication between the players and the server.
 */
 package main
